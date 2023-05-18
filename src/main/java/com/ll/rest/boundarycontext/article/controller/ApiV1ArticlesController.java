97087b4030a6539a689953cc3c326c69dc7bde00
Jzakka <mouse4786@gmail.com>
2023-05-18 17:30:13 +0900

    글 수정&삭제

--- src/main/java/com/ll/rest/boundarycontext/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/ll/rest/boundarycontext/article/controller/ApiV1ArticlesController.java
@@ -4,17 +4,22 @@ import com.ll.rest.base.rsData.RsData;
 import com.ll.rest.boundarycontext.article.entity.Article;
 import com.ll.rest.boundarycontext.article.service.ArticleService;
 import com.ll.rest.boundarycontext.member.entity.Member;
+import com.ll.rest.boundarycontext.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -24,6 +29,40 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Tag(name = "ApiV1ArticlesController", description = "게시물 CRUD 컨트롤러")
 public class ApiV1ArticlesController {
     private final ArticleService articleService;
+    private final MemberService memberService;
+
+    @Data
+    public static class WriteRequest {
+        @NotBlank
+        private String subject;
+        @NotBlank
+        private String content;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class WriteResponse {
+        private final Article article;
+    }
+
+    @PostMapping
+    @Operation(summary = "등록", security = @SecurityRequirement(name = "bearerAuth"))
+    public RsData<WriteResponse> write(
+            @AuthenticationPrincipal User user,
+            @Valid @RequestBody WriteRequest writeRequest) {
+        Member member = memberService.findByUsername(user.getUsername()).orElseThrow();
+        RsData<Article> writeRs = articleService.write(member, writeRequest.getSubject(), writeRequest.getContent());
+
+        if (writeRs.isFail()) {
+            return (RsData) writeRs;
+        }
+
+        return RsData.of(
+                writeRs.getResultCode(),
+                writeRs.getMsg(),
+                new WriteResponse(writeRs.getData())
+        );
+    }
 
     @AllArgsConstructor
     @Getter
@@ -56,10 +95,56 @@ public class ApiV1ArticlesController {
                 "S-1",
                 "성공",
                 new ArticleResponse(article)
-        )).orElseGet(()->RsData.of(
+        )).orElseGet(() -> RsData.of(
                 "F-1",
                 "%d번 게시물은 존재하지 않습니다.".formatted(id),
                 null
         ));
+    }
+
+    @Data
+    public static class ModifyRequest {
+        @NotBlank
+        private String subject;
+        @NotBlank
+        private String content;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class ModifyResponse {
+        private final Article article;
+    }
+
+    @PatchMapping(value = "/{id}", consumes = APPLICATION_JSON_VALUE)
+    @Operation(summary = "수정", security = @SecurityRequirement(name = "bearerAuth"))
+    public RsData<ModifyResponse> modify(
+            @AuthenticationPrincipal User user,
+            @Valid @RequestBody ModifyRequest modifyRequest,
+            @PathVariable Long id) {
+        Member member = memberService.findByUsername(user.getUsername()).orElseThrow();
+
+        Optional<Article> optionalArticle = articleService.findById(id);
+
+        if (optionalArticle.isEmpty()) {
+            return RsData.of(
+                    "F-1",
+                    "%d번 게시물은 존재하지 않습니다.".formatted(id),
+                    null);
+        }
+
+        RsData canModify = articleService.canModify(member, optionalArticle.get());
+
+        if (canModify.isFail()) {
+            return canModify;
+        }
+
+        RsData<Article> modifyRs = articleService.modify(optionalArticle.get(), modifyRequest.getSubject(), modifyRequest.getContent());
+
+        return RsData.of(
+                modifyRs.getResultCode(),
+                modifyRs.getMsg(),
+                new ModifyResponse(modifyRs.getData())
+        );
     }
 }
